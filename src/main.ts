637b3bffb97930a1cf688c31cb779ee56082b03d
madhead <siarhei.krukau@gmail.com>
2021-03-24 23:14:02 +0300

    Debug the input

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     const version: string = core.getInput('version')
+    const compareTo: string = core.getInput('compare-to')
 
     core.info(`Version: ${version}`)
+    core.info(`Compare to: ${compareTo}`)
   } catch (error) {
     core.setFailed(error.message)
   }
