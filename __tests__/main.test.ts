2f161dea8358cc2e61eb261a7296c0df6f8d5d42
madhead <siarhei.krukau@gmail.com>
2021-03-25 00:02:11 +0300

    Fix #13: Implement the action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,13 +1,88 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import { execPath } from 'process'
+import { execFileSync } from 'child_process'
+import { join } from 'path'
 
-test('test runs', () => {
-  // process.env['INPUT_MILLISECONDS'] = '500'
-  // const np = process.execPath
-  // const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  // const options: cp.ExecFileSyncOptions = {
-  //   env: process.env
-  // }
-  // console.log(cp.execFileSync(np, [ip], options).toString())
+const data = [
+  { version: 'Forty-two', valid: false },
+  { version: '1.2', valid: false },
+  { version: '1', valid: false },
+  { version: '1', valid: false, compareTo: '0.0.0' },
+  { version: '0.0.0', valid: true, major: 0, minor: 0, patch: 0 },
+  { version: '0.0.1', valid: true, major: 0, minor: 0, patch: 1 },
+  { version: '0.1.1', valid: true, major: 0, minor: 1, patch: 1 },
+  { version: '1.0.0', valid: true, major: 1, minor: 0, patch: 0 },
+  { version: '1.2.3', valid: true, major: 1, minor: 2, patch: 3 },
+
+  {
+    version: '1.2.3',
+    valid: true,
+    compareTo: '0.0',
+    compareToValid: false,
+    major: 1,
+    minor: 2,
+    patch: 3
+  },
+  {
+    version: '1.2.3',
+    valid: true,
+    compareTo: '0.0.0',
+    compareToValid: true,
+    major: 1,
+    minor: 2,
+    patch: 3,
+    comparisonResult: '>'
+  },
+  {
+    version: '1.2.3',
+    valid: true,
+    compareTo: '1.2.3',
+    compareToValid: true,
+    major: 1,
+    minor: 2,
+    patch: 3,
+    comparisonResult: '='
+  },
+  {
+    version: '1.2.3',
+    valid: true,
+    compareTo: '2.0.0',
+    compareToValid: true,
+    major: 1,
+    minor: 2,
+    patch: 3,
+    comparisonResult: '<'
+  }
+]
+
+data.forEach(item => {
+  test(`${item.version} / ${item.compareTo}`, () => {
+    const stdout = execFileSync(
+      execPath,
+      [join(__dirname, '..', 'lib', 'main.js')],
+      {
+        env: {
+          'INPUT_VERSION': item.version,
+          'INPUT_COMPARE-TO': item.compareTo
+        }
+      }
+    )
+      .toString()
+
+    if (item.valid) {
+      if (item.compareToValid) {
+        expect(stdout).toBe(`::set-output name=major::${item.major}
+::set-output name=minor::${item.minor}
+::set-output name=patch::${item.patch}
+::set-output name=comparison-result::${item.comparisonResult}
+`)
+      } else {
+        expect(stdout).toBe(`::set-output name=major::${item.major}
+::set-output name=minor::${item.minor}
+::set-output name=patch::${item.patch}
+`)
+      }
+    } else {
+      expect(stdout).toBe('')
+    }
+  })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
 import * as core from '@actions/core'
+import {parse} from 'semver'
 
 async function run(): Promise<void> {
   try {
-    const version: string = core.getInput('version')
-    const compareTo: string = core.getInput('compare-to')
+    const versionInput = core.getInput('version')
+    const version = parse(versionInput)
 
-    core.info(`Version: ${version}`)
-    core.info(`Compare to: ${compareTo}`)
+    if (version == null) {
+      return
+    }
+
+    core.setOutput('major', version.major)
+    core.setOutput('minor', version.minor)
+    core.setOutput('patch', version.patch)
+
+    const compareToInput: string = core.getInput('compare-to')
+    const compareTo = parse(compareToInput)
+
+    if (compareTo == null) {
+      return
+    }
+
+    switch (version.compare(compareTo)) {
+      case -1:
+        core.setOutput('comparison-result', '<')
+        break
+      case 0:
+        core.setOutput('comparison-result', '=')
+        break
+      case 1:
+        core.setOutput('comparison-result', '>')
+        break
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
