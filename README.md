2c85bea09cd5887be852f94106aac3ba755b8e45
madhead <siarhei.krukau@gmail.com>
2021-03-27 03:35:49 +0300

    Fix #36: Support for build parts

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 # madhead/semver-utils
 
 One-stop shop for working with semantic versions in your GitHub Actions workflows.
+A wrapper around [semver](https://www.npmjs.com/package/semver).
 
 ## Usage
 
@@ -15,7 +16,7 @@ One-stop shop for working with semantic versions in your GitHub Actions workflow
   id: version
   with:
     # A version to work with
-    version: 1.2.3
+    version: 1.2.3+42.24
 
     # A version to compare against
     compare-to: 2.1.0
@@ -26,13 +27,21 @@ One-stop shop for working with semantic versions in your GitHub Actions workflow
     echo "${{ steps.version.outputs.major }}"             # 1
     echo "${{ steps.version.outputs.minor }}"             # 2
     echo "${{ steps.version.outputs.patch }}"             # 3
-
+    echo "${{ steps.version.outputs.build }}"             # 42.24
+    echo "${{ steps.version.outputs.build-parts }}"       # 2
+    echo "${{ steps.version.outputs.build-0 }}"           # 42
+    echo "${{ steps.version.outputs.build-1 }}"           # 24
     echo "${{ steps.version.outputs.comparison-result }}" # <
-
     echo "${{ steps.version.outputs.satisfies }}"         # true
 ```
 
 If any of the inputs cannot be parsed, it is just silently ignored.
 This action tries its best not to fail.
 
-See how this action uses itself to check if a PR to main increments the version: [`pr_main.yml`](.github/workflows/pr_main.yml)
+To see the list of available versions (`@latest` in the example above), navigate to the [Releases & Tags](https://github.com/madhead/semver-utils/tags) page of this repo.
+
+To learn more the inputs / outpus look at the comprehensive test suit: [`main.test.ts`](__tests__/main.test.ts).
+
+To see this action… in action check its integration test: [`default.yml`](.github/workflows/default.yml).
+
+See how this action uses itself to check if a PR to the `main` branch increments the version: [`pr_main.yml`](.github/workflows/pr_main.yml).
--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,129 +1,183 @@
-import { execPath } from 'process'
-import { execFileSync } from 'child_process'
-import { join } from 'path'
-import { coerce } from 'semver'
+import {execPath} from 'process'
+import {execFileSync} from 'child_process'
+import {join} from 'path'
+import {parse, satisfies} from 'semver'
 
-const data = [
-  { version: 'Forty-two', valid: false },
-  { version: '1.2', valid: false },
-  { version: '1', valid: false },
-  { version: '1', valid: false, compareTo: '0.0.0' },
-  { version: '0.0.0', valid: true, major: 0, minor: 0, patch: 0 },
-  { version: '0.0.1', valid: true, major: 0, minor: 0, patch: 1 },
-  { version: '0.1.1', valid: true, major: 0, minor: 1, patch: 1 },
-  { version: '1.0.0', valid: true, major: 1, minor: 0, patch: 0 },
-  { version: '1.2.3', valid: true, major: 1, minor: 2, patch: 3 },
+interface TestData {
+  version: string
+  compareTo?: string
+  satisfies?: string
+}
 
-  {
-    version: '1.2.3',
-    valid: true,
-    compareTo: '0.0',
-    compareToValid: false,
-    major: 1,
-    minor: 2,
-    patch: 3
-  },
-  {
-    version: '1.2.3',
-    valid: true,
-    compareTo: '0.0.0',
-    compareToValid: true,
-    major: 1,
-    minor: 2,
-    patch: 3,
-    comparisonResult: '>'
-  },
-  {
-    version: '1.2.3',
-    valid: true,
-    compareTo: '1.2.3',
-    compareToValid: true,
-    major: 1,
-    minor: 2,
-    patch: 3,
-    comparisonResult: '='
-  },
-  {
-    version: '1.2.3',
-    valid: true,
-    compareTo: '2.0.0',
-    compareToValid: true,
-    major: 1,
-    minor: 2,
-    patch: 3,
-    comparisonResult: '<'
-  }
+const data: TestData[] = [
+  {version: 'Forty-two'},
+  {version: '1.2'},
+  {version: '1'},
+
+  {version: '0.0.0', satisfies: ''},
+  {version: '0.0.0', compareTo: '0.0.0', satisfies: '*'},
+  {version: '0.0.0', compareTo: '0.0'},
+  {version: '0.0.0', compareTo: '0'},
+  {version: '0.0.0', compareTo: ''},
+  {version: '0.0.0', compareTo: 'Forty-two'},
+  {version: '0.0.1'},
+  {version: '0.0.1', compareTo: '0.0.2'},
+  {version: '0.1.2'},
+  {version: '0.1.2', compareTo: '0.2.0'},
+  {version: '0.1.2', compareTo: '0.1.1'},
+  {version: '1.2.3'},
+  {version: '1.2.3', compareTo: '1.2.3'},
+  {version: '1.2.3', compareTo: '1.2.3+42'},
+  {version: '1.2.3', compareTo: '1.2.3+42.24'},
+  {version: '1.2.3', compareTo: '1.2.4'},
+  {version: '1.2.3', compareTo: '1.3.0'},
+  {version: '1.2.3', compareTo: '2.0.0'},
+  {version: '1.2.3', compareTo: '1.2.2'},
+  {version: '1.2.3', compareTo: '1.1.99'},
+  {version: '1.2.3', compareTo: '0.99.99'},
+  {version: '1.2.3', compareTo: ''},
+  {version: '1.2.3', compareTo: '    '},
+  {version: '1.2.3', compareTo: 'XXX'},
+  {version: '1.2.3', satisfies: '>1.0.0'},
+  {version: '1.2.3', satisfies: '<2.0.0'},
+  {version: '1.2.3', satisfies: '>=1.2.3'},
+  {version: '1.2.3', satisfies: '<=1.2.3'},
+  {version: '1.2.3', satisfies: '=1.2.3'},
+  {version: '1.2.3', satisfies: '>1.0.0 <2.0.0'},
+  {version: '1.2.3', satisfies: '1.2.x'},
+  {version: '1.2.3', satisfies: '1.x'},
+  {version: '2.2.3', satisfies: '<2.0.0'},
+  {version: '2.2.3', satisfies: '<=2.0.0'},
+  {version: '2.2.3', satisfies: '=1.2.3'},
+  {version: '2.2.3', satisfies: '>1.0.0 < 2.0.0'},
+  {version: '2.2.3', satisfies: '1.2.x'},
+  {version: '2.2.3', satisfies: '1.x'},
+  {version: '2.2.3', satisfies: ''},
+  {version: '2.2.3', satisfies: ' '},
+  {version: '2.2.3', satisfies: '  '},
+  {version: '2.2.3', satisfies: 'XXX'},
+  {version: '1.2.3+alpha'},
+  {version: '1.2.3+alpha.beta'},
+  {version: '1.2.3+42'},
+  {version: '1.2.3+42.24'}
 ]
 
-data.forEach(item => {
-  test(`${item.version} / ${item.compareTo}`, () => {
-    const stdout = execFileSync(
-      execPath,
-      [join(__dirname, '..', 'lib', 'main.js')],
-      {
-        env: {
-          INPUT_VERSION: item.version,
-          'INPUT_COMPARE-TO': item.compareTo
-        }
-      }
-    ).toString()
+function validate(env: NodeJS.ProcessEnv, validate: (stdout: string) => void) {
+  validate(
+    execFileSync(execPath, [join(__dirname, '..', 'lib', 'main.js')], {
+      env: env
+    }).toString()
+  )
+}
 
-    if (item.valid) {
-      if (item.compareToValid) {
-        expect(stdout).toBe(`::set-output name=major::${item.major}
-::set-output name=minor::${item.minor}
-::set-output name=patch::${item.patch}
-::set-output name=comparison-result::${item.comparisonResult}
-`)
-      } else {
-        expect(stdout).toBe(`::set-output name=major::${item.major}
-::set-output name=minor::${item.minor}
-::set-output name=patch::${item.patch}
-`)
-      }
-    } else {
-      expect(stdout).toBe('')
+describe('parse', () => {
+  data.forEach(item => {
+    test(`parse(${item.version})`, () => {
+      validate(
+        {
+          INPUT_VERSION: item.version
+        },
+        (stdout: string) => {
+          const parsedVersion = parse(item.version)
+
+          if (parsedVersion) {
+            expect(stdout).toContain(
+              `::set-output name=major::${parsedVersion.major}`
+            )
+            expect(stdout).toContain(
+              `::set-output name=minor::${parsedVersion.minor}`
+            )
+            expect(stdout).toContain(
+              `::set-output name=patch::${parsedVersion.patch}`
+            )
+            if (parsedVersion.build.length > 0) {
+              expect(stdout).toContain(
+                `::set-output name=build::${parsedVersion.build.join('.')}`
+              )
+              expect(stdout).toContain(
+                `::set-output name=build-parts::${parsedVersion.build.length}`
+              )
+              parsedVersion.build.forEach((buildPart, index) => {
+                ;`::set-output name=build-${index}::${buildPart}`
+              })
+            }
+          } else {
+            expect(stdout).not.toContain(`::set-output name=major::`)
+            expect(stdout).not.toContain(`::set-output name=minor::`)
+            expect(stdout).not.toContain(`::set-output name=patch::`)
+            expect(stdout).not.toContain(`::set-output name=build::`)
+            expect(stdout).not.toContain(`::set-output name=build[::`)
+          }
+        }
+      )
+    })
+  })
+})
+
+describe('compare', () => {
+  data.forEach(item => {
+    if (item.compareTo !== undefined) {
+      test(`compare(${item.version}, ${item.compareTo})`, () => {
+        validate(
+          {
+            INPUT_VERSION: item.version,
+            'INPUT_COMPARE-TO': item.compareTo
+          },
+          (stdout: string) => {
+            if (item.compareTo) {
+              const parsedCompareTo = parse(item.compareTo)
+
+              if (parsedCompareTo) {
+                if (parsedCompareTo.compare(item.version) == 0) {
+                  expect(stdout).toContain(
+                    `::set-output name=comparison-result::=`
+                  )
+                } else if (parsedCompareTo.compare(item.version) == -1) {
+                  expect(stdout).toContain(
+                    `::set-output name=comparison-result::>`
+                  )
+                } else if (parsedCompareTo.compare(item.version) == 1) {
+                  expect(stdout).toContain(
+                    `::set-output name=comparison-result::<`
+                  )
+                }
+              }
+            } else {
+              expect(stdout).not.toContain(
+                `::set-output name=comparison-result::`
+              )
+            }
+          }
+        )
+      })
     }
   })
 })
 
-const satisfiesData = [
-  { version: '1.2.3', range: '', result: undefined },
+describe('satisfies', () => {
+  data.forEach(item => {
+    if (item.satisfies !== undefined) {
+      test(`satisfies(${item.version}, ${item.satisfies})`, () => {
+        validate(
+          {
+            INPUT_VERSION: item.version,
+            INPUT_SATISFIES: item.satisfies
+          },
+          (stdout: string) => {
+            const parsedVersion = parse(item.version)
 
-  { version: '1.2.3', range: '*', result: true },
-  { version: '1.2.3', range: '>1.0.0', result: true },
-  { version: '1.2.3', range: '<2.0.0', result: true },
-  { version: '1.2.3', range: '>=1.2.3', result: true },
-  { version: '1.2.3', range: '<=1.2.3', result: true },
-  { version: '1.2.3', range: '=1.2.3', result: true },
-  { version: '1.2.3', range: '>1.0.0 <2.0.0', result: true },
-  { version: '1.2.3', range: '1.2.x', result: true },
-  { version: '1.2.3', range: '1.x', result: true },
-
-  { version: '2.2.3', range: '<2.0.0', result: false },
-  { version: '2.2.3', range: '=1.2.3', result: false },
-  { version: '2.2.3', range: '>1.0.0 <2.0.0', result: false },
-  { version: '2.2.3', range: '1.2.x', result: false },
-  { version: '2.2.3', range: '1.x', result: false },
-]
-
-satisfiesData.forEach(item => {
-  test(`satisfies(${item.version}, ${item.range})`, () => {
-    const stdout = execFileSync(
-      execPath,
-      [join(__dirname, '..', 'lib', 'main.js')],
-      {
-        env: {
-          INPUT_VERSION: item.version,
-          INPUT_SATISFIES: item.range
-        }
-      }
-    ).toString()
-
-    if (item.result !== undefined) {
-      expect(stdout).toContain(`::set-output name=satisfies::${item.result}`)
-    } else {
-      expect(stdout).not.toContain('satisfies')
+            if (parsedVersion && item.satisfies?.trim() != '') {
+              if (satisfies(parsedVersion, item.satisfies!!)) {
+                expect(stdout).toContain(`::set-output name=satisfies::true`)
+              } else {
+                expect(stdout).toContain(`::set-output name=satisfies::false`)
+              }
+            } else {
+              expect(stdout).not.toContain(`::set-output name=satisfies::`)
+            }
+          }
+        )
+      })
     }
   })
 })
