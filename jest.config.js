8a8e24400603b6a856eb51dae7aaf92cbdb9097b
madhead <siarhei.krukau@gmail.com>
2026-05-12 04:27:22 +0200

    Fix #847: Node 24 + some updates

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': ['ts-jest', { tsconfig: { types: ['node', 'jest'] } }]
   },
   verbose: true
 }
